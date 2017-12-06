c1c20fba059b2380c95d602e9163b6b2ebe4ac66
Daniel Selans <daniel.selans@gmail.com>
2017-12-05 22:53:12 -0800

    some tests for http

--- checkers/http_test.go
+++ checkers/http_test.go
@@ -1,6 +1,8 @@
 package checkers
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
@@ -34,5 +36,119 @@ func TestNewHTTP(t *testing.T) {
 
 		Expect(h).To(BeNil())
 		Expect(err.Error()).To(ContainSubstring("URL cannot be nil"))
+	})
+}
+
+func TestStatus(t *testing.T) {
+	RegisterTestingT(t)
+
+	t.Run("Happy path", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer ts.Close()
+
+		testURL, err := url.Parse(ts.URL)
+		Expect(err).ToNot(HaveOccurred())
+
+		cfg := &HTTPConfig{
+			URL: testURL,
+		}
+
+		checker, err := NewHTTP(cfg)
+		Expect(err).ToNot(HaveOccurred())
+
+		data, err := checker.Status()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(data).To(BeNil())
+	})
+
+	t.Run("Should return error if HTTP call fails", func(t *testing.T) {
+		testURL, _ := url.Parse("no-scheme.com")
+
+		cfg := &HTTPConfig{
+			URL: testURL,
+		}
+
+		checker, err := NewHTTP(cfg)
+		Expect(err).ToNot(HaveOccurred())
+
+		data, err := checker.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("unsupported protocol"))
+		Expect(data).To(BeNil())
+	})
+
+	t.Run("Should return error if expected response status code does not match received status code", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+		defer ts.Close()
+
+		testURL, err := url.Parse(ts.URL)
+		Expect(err).ToNot(HaveOccurred())
+
+		cfg := &HTTPConfig{
+			URL: testURL,
+		}
+
+		checker, err := NewHTTP(cfg)
+		Expect(err).ToNot(HaveOccurred())
+
+		data, err := checker.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("does not match expected status code"))
+		Expect(data).To(BeNil())
+	})
+
+	t.Run("Should return error if response data does not contain expected data", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("foo"))
+		}))
+		defer ts.Close()
+
+		testURL, err := url.Parse(ts.URL)
+		Expect(err).ToNot(HaveOccurred())
+
+		cfg := &HTTPConfig{
+			URL:    testURL,
+			Expect: "bar",
+		}
+
+		checker, err := NewHTTP(cfg)
+		Expect(err).ToNot(HaveOccurred())
+
+		data, err := checker.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("does not contain expected content"))
+		Expect(data).To(BeNil())
+	})
+
+	t.Run("Should not error if expected data in response is found", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("foo"))
+		}))
+		defer ts.Close()
+
+		testURL, err := url.Parse(ts.URL)
+		Expect(err).ToNot(HaveOccurred())
+
+		cfg := &HTTPConfig{
+			URL:    testURL,
+			Expect: "foo",
+		}
+
+		checker, err := NewHTTP(cfg)
+		Expect(err).ToNot(HaveOccurred())
+
+		data, err := checker.Status()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(data).To(BeNil())
+	})
+
+	t.Run("Should return error if response body is not readable", func(t *testing.T) {
+
 	})
 }
