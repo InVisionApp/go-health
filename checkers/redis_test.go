3e32b0c4194ffcc08552bb8c2695b6d05c77da2e
Daniel Selans <daniel.selans@gmail.com>
2017-12-16 20:27:32 -0800

    complete redis checker tests

--- checkers/redis_test.go
+++ checkers/redis_test.go
@@ -1,9 +1,11 @@
 package checkers
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/go-redis/redis"
+	"github.com/alicebob/miniredis"
+	redis "github.com/go-redis/redis"
 	. "github.com/onsi/gomega"
 )
 
@@ -111,38 +113,206 @@ func TestRedisStatus(t *testing.T) {
 	RegisterTestingT(t)
 
 	t.Run("Should error when ping is enabled and fails", func(t *testing.T) {
+		cfg := &RedisConfig{
+			Ping: true,
+		}
+		checker, server, err := setupRedis(cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
 
+		// Stop the server, so ping check fails
+		server.Close()
+
+		Expect(err).ToNot(HaveOccurred())
+
+		_, err = checker.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Ping failed"))
 	})
 
 	t.Run("When set is enabled", func(t *testing.T) {
 		t.Run("should error if set fails", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Set: &RedisSetOptions{
+					Key: "test-key",
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
 
+			// Stop the server, so ping check fails
+			server.Close()
+
+			_, err = checker.Status()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Unable to complete set"))
 		})
 
 		t.Run("should use .Value if .Value is defined", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Set: &RedisSetOptions{
+					Key:   "test-key",
+					Value: "test-value",
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
 
+			_, err = checker.Status()
+			Expect(err).ToNot(HaveOccurred())
+
+			val, err := server.Get(cfg.Set.Key)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(val).To(Equal(cfg.Set.Value))
+		})
+
+		t.Run("should use default .Value if .Value is not explicitly set", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Set: &RedisSetOptions{
+					Key: "test-key",
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
+
+			_, err = checker.Status()
+			Expect(err).ToNot(HaveOccurred())
+
+			val, err := server.Get(cfg.Set.Key)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(val).To(Equal(RedisDefaultSetValue))
 		})
 	})
 
 	t.Run("When get is enabled", func(t *testing.T) {
 		t.Run("should error if key is missing and NoErrorMissingKey not set", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Get: &RedisGetOptions{
+					Key: "test-key",
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
 
+			_, err = checker.Status()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("Unable to complete get: '%v' not found", cfg.Get.Key)))
 		})
 
 		t.Run("should NOT error if key is missing and NoErrorMissingKey IS set", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Get: &RedisGetOptions{
+					Key:               "test-key",
+					NoErrorMissingKey: true,
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
 
+			_, err = checker.Status()
+			Expect(err).ToNot(HaveOccurred())
 		})
 
 		t.Run("should error if get fails", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Get: &RedisGetOptions{
+					Key:               "test-key",
+					NoErrorMissingKey: true,
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
 
+			// Close the server so the GET fails
+			server.Close()
+
+			_, err = checker.Status()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Unable to complete get"))
 		})
 
 		t.Run("should error if .Expect is set and the value does not match", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Set: &RedisSetOptions{
+					Key:   "test-key",
+					Value: "foo",
+				},
+				Get: &RedisGetOptions{
+					Key:               "test-key",
+					Expect:            "bar",
+					NoErrorMissingKey: true,
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
 
+			_, err = checker.Status()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("does not match expected value"))
 		})
 
 		t.Run("should NOT error if .Expect is not set", func(t *testing.T) {
+			cfg := &RedisConfig{
+				Set: &RedisSetOptions{
+					Key:   "test-key",
+					Value: "foo",
+				},
+				Get: &RedisGetOptions{
+					Key: "test-key",
+				},
+			}
+			checker, server, err := setupRedis(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer server.Close()
 
+			_, err = checker.Status()
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
+}
+
+func setupRedis(cfg *RedisConfig) (*Redis, *miniredis.Miniredis, error) {
+	server, err := miniredis.Run()
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to setup miniredis: %v", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: server.Addr(),
+	})
+
+	if _, err := client.Ping().Result(); err != nil {
+		return nil, nil, fmt.Errorf("Unable to complete ping during setup: %v", err)
+	}
+
+	cfg.Client = client
+
+	checker, err := NewRedis(cfg)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to setup checker: %v", err)
+	}
+
+	return checker, server, nil
 }
