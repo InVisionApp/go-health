a454b98538ac8c9a5e15e31570ea80d63125c1f5
Al Ganiev <helios.ag@gmail.com>
2018-10-14 09:45:02 +1000

    Added checker for disk usage (in percent)

---
+++ checkers/disk_usage.go
@@ -0,0 +1,80 @@
+package checkers
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/disk"
+)
+
+// DiskUsageConfig is used for configuring the go-diskusage check.
+//
+// "Path" is _required_; path to check directory/drive (ex. /home/user)
+// "WarningThreshold" is _required_; set percent (more than 0 and less 100) of free space at specified path,
+//  which triggers warning.
+// "CriticalThreshold" is _required_; set percent (more than 0 and less 100) of free space at specified path,
+//  which triggers critical.
+type DiskUsageConfig struct {
+	Path string
+	WarningThreshold float64
+	CriticalThreshold float64
+}
+
+// DiskUsage implements the "ICheckable" interface.
+type DiskUsage struct {
+	Config *DiskUsageConfig
+}
+
+func NewDiskUsage(cfg *DiskUsageConfig) (*DiskUsage, error)  {
+	if cfg == nil {
+		return nil, fmt.Errorf("Passed in config cannot be nil")
+	}
+
+	if err := validateDiskUsageConfig(cfg); err != nil {
+		return nil, fmt.Errorf("Unable to validate diskusage config: %v", err)
+	}
+
+	return &DiskUsage{
+		Config: cfg,
+	}, nil
+}
+
+// Status is used for performing a diskusage check against a dependency; it satisfies
+// the "ICheckable" interface.
+func (d *DiskUsage) Status() (interface{}, error) {
+	stats, err := disk.Usage(d.Config.Path)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error getting disk usage: %v", err)
+	}
+
+	diskUsage := stats.UsedPercent
+
+	if diskUsage >= d.Config.CriticalThreshold {
+		return nil, fmt.Errorf("Critical: disk usage too high %.2f percent", diskUsage)
+	}
+
+	if diskUsage >= d.Config.WarningThreshold {
+		return nil, fmt.Errorf("Warning: disk usage too high %.2f percent", diskUsage)
+	}
+
+	return nil, nil
+}
+
+func validateDiskUsageConfig(cfg *DiskUsageConfig) error {
+	if cfg == nil {
+		return fmt.Errorf("Main config cannot be nil")
+	}
+
+	if cfg.Path == "" {
+		return fmt.Errorf("Path can not be nil")
+	}
+
+	if cfg.WarningThreshold > 100.0 || cfg.WarningThreshold < 0 {
+		return fmt.Errorf("Invalid warning threshold value (more 100 or less 0)")
+	}
+
+	if cfg.CriticalThreshold > 100.0 || cfg.CriticalThreshold < 0 {
+		return fmt.Errorf("Invalid critical threshold value (more 100 or less 0)")
+	}
+
+	return nil
+}
---
+++ checkers/disk_usage_test.go
@@ -0,0 +1,139 @@
+package checkers
+
+import (
+	. "github.com/onsi/gomega"
+	"os"
+	"testing"
+)
+
+func TestNewDiskUsage(t *testing.T) {
+	RegisterTestingT(t)
+
+	t.Run("Happy path and set thresholds", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: os.TempDir(),
+			WarningThreshold: 5,
+			CriticalThreshold: 5,
+		}
+
+		du, err := NewDiskUsage(cfg)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(du).ToNot(BeNil())
+
+	})
+
+	t.Run("Should error with a nil cfg", func(t *testing.T) {
+		du, err := NewDiskUsage(nil)
+
+		Expect(du).To(BeNil())
+		Expect(err.Error()).To(ContainSubstring("Passed in config cannot be nil"))
+	})
+
+	t.Run("Bad config should error", func(t *testing.T) {
+		du, err := NewDiskUsage(&DiskUsageConfig{})
+
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Unable to validate diskusage config"))
+		Expect(du).To(BeNil())
+	})
+}
+
+func TestValidateDiskUsageConfig(t *testing.T) {
+	RegisterTestingT(t)
+
+	t.Run("Should error with nil main config", func(t *testing.T) {
+		var cfg *DiskUsageConfig
+		err := validateDiskUsageConfig(cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Main config cannot be nil"))
+	})
+
+	t.Run("Config must have path set", func(t *testing.T) {
+		cfg := &DiskUsageConfig{}
+
+		err := validateDiskUsageConfig(cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Path can not be nil"))
+	})
+
+	t.Run("Should error if warning threshold value set out of bounds", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: os.TempDir(),
+			WarningThreshold: -1,
+			CriticalThreshold: 100,
+		}
+
+		err := validateDiskUsageConfig(cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Invalid warning threshold value"))
+	})
+
+	t.Run("Should error if critical threshold value set out of bounds", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: os.TempDir(),
+			WarningThreshold: 10,
+			CriticalThreshold: 101,
+		}
+
+		err := validateDiskUsageConfig(cfg)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Invalid critical threshold value"))
+	})
+
+}
+
+func TestDiskUsageStatus(t *testing.T) {
+	RegisterTestingT(t)
+
+	t.Run("Should error when critical threshold reached", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: "/unknown/path",
+			WarningThreshold: 50.0,
+			CriticalThreshold: 50.0,
+		}
+
+		du, err := NewDiskUsage(cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = du.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Error getting disk usage"))
+	})
+
+	t.Run("Should error when critical threshold reached", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: os.TempDir(),
+			WarningThreshold: 90,
+			CriticalThreshold: 1,
+		}
+
+		du, err := NewDiskUsage(cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = du.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Critical: disk usage too high"))
+	})
+
+
+	t.Run("Should error when warning threshold reached", func(t *testing.T) {
+		cfg := &DiskUsageConfig{
+			Path: os.TempDir(),
+			WarningThreshold: 1,
+			CriticalThreshold: 99,
+		}
+
+		du, err := NewDiskUsage(cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = du.Status()
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Warning: disk usage too high"))
+	})
+
+}
